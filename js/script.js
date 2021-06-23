2df40b321410b52bee546d8d37967fa96188cb96
Flavio <flavio.origgi@outlook.it>
2021-06-23 17:13:59 +0200

    inizio JS

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const app = new Vue(
+    {
+        el: '#app',
+        data:{
+            newList: '',
+            list: [
+                'Studiare boolean',                
+                'fare pulizie casalinghe',
+                'Andare in palestra',
+                'Uscire a camminare'
+            ] 
+        }
+
+    }
+);
